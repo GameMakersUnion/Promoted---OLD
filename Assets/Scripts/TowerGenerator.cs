be466bdd6dc8bfb320a88bab8ab0e6f5ab3912e4
LiamAttClarke <liam.att.clarke@gmail.com>
2015-05-01 11:57:58 -0400

    **Initial commit** Added tower generator; Organized folder structure;

---
+++ Assets/Scripts/TowerGenerator.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TowerGenerator : MonoBehaviour {
+	public int floors = 12;
+	public GameObject[] levelsA, levelsB, levelsC;
+	public GameObject levelFinal;
+	float scaleFactor = 4;
+
+	// Use this for initialization
+	void Start () {
+		floors = floors - (floors % 3);
+		GenerateTower ();
+	}
+
+	void GenerateTower () {
+		GameObject level;
+		int rand;
+		for (int i = 0; i < floors; i++) {
+			if (i < floors / 3) {
+				rand = Random.Range (0, levelsA.Length);
+				AddFloor(levelsA[rand], i);
+			} else if (i < floors / 3 * 2) {
+				rand = Random.Range (0, levelsB.Length);
+				AddFloor(levelsB[rand], i);
+			} else if (i < floors) {
+				rand = Random.Range (0, levelsC.Length);
+				AddFloor(levelsC[rand], i);
+			}
+		}
+		AddFloor (levelFinal, floors);
+	}
+
+	void AddFloor (GameObject level, int height) {
+		level = (GameObject)GameObject.Instantiate (level);
+		level.transform.position = new Vector3 (0,height * scaleFactor,0);
+		level.transform.parent = transform;
+	}
+}
